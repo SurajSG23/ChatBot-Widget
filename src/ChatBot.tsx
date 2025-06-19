c1042f742b5f83f9d9a626f294af14b08e01f66d
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-06-20 05:13:05 +0530

    Update package dependencies and refine ChatBot styling

--- src/ChatBot.tsx
+++ src/ChatBot.tsx
@@ -97,14 +97,9 @@ const Chatbot: React.FC = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
-  useEffect(() => {
-    const path = window.location.href;
-    if (path.includes("local")) setProject("project-three");
-  }, []);
-
   return (
     <div className="chatbot-container-main">
-      <div onClick={toggleChatbot}>
+      {/* <div onClick={toggleChatbot}>
         <ul className="wrapper">
           <li className="icon facebook">
             {!isOpen ? <span className="tooltip">NEED HELP?</span> : ""}
@@ -116,191 +111,223 @@ const Chatbot: React.FC = () => {
             )}
           </li>
         </ul>
-      </div>
+      </div> */}
 
-      {isOpen && (
-        <div className="chatbot-container">
-          <div className="chatbot-header">
-            <select
-              name="model"
-              id="model-selection"
-              onChange={(e) => {
-                setModel(e.target.value);
-              }}
+      {/* {isOpen && ( */}
+      <div className="chatbot-container">
+        <div className="chatbot-header">
+          <select
+            name="model"
+            id="model-selection"
+            onChange={(e) => {
+              setModel(e.target.value);
+            }}
+          >
+            <option value="qwen3-32b-genwai" hidden>
+              Select Model &#11167;
+            </option>
+
+            <option value="qwen3-32b-genwai">Qwen3-32b-genwai</option>
+            <option value="Qwen/Qwen2.5-Coder-32B-Instruct-AWQ">
+              Qwen/Qwen2.5-Coder
+            </option>
+          </select>
+
+          <button className="bin-button" onClick={clearChat}>
+            <span className="tooltiptext">Clear Chat</span>
+            <svg
+              className="bin-top"
+              viewBox="0 0 39 7"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <option value="qwen3-32b-genwai" hidden>
-                Select Model &#11167;
-              </option>
-
-              <option value="qwen3-32b-genwai">Qwen3-32b-genwai</option>
-              <option value="Qwen/Qwen2.5-Coder-32B-Instruct-AWQ">
-                Qwen/Qwen2.5-Coder
-              </option>
-            </select>
-
-            <button className="bin-button" onClick={clearChat}>
-              <span className="tooltiptext">Clear Chat</span>
-              <svg
-                className="bin-top"
-                viewBox="0 0 39 7"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <line
-                  y1="5"
-                  x2="39"
-                  y2="5"
-                  stroke="white"
-                  strokeWidth="4"
-                ></line>
-                <line
-                  x1="12"
-                  y1="1.5"
-                  x2="26.0357"
-                  y2="1.5"
-                  stroke="white"
-                  strokeWidth="3"
-                ></line>
-              </svg>
-              <svg
-                className="bin-bottom"
-                viewBox="0 0 33 39"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <mask id="path-1-inside-1_8_19" fill="white">
-                  <path d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"></path>
-                </mask>
-                <path
-                  d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
-                  fill="white"
-                  mask="url(#path-1-inside-1_8_19)"
-                ></path>
-                <path d="M12 6L12 29" stroke="white" strokeWidth="4"></path>
-                <path d="M21 6V29" stroke="white" strokeWidth="4"></path>
-              </svg>
-            </button>
-          </div>
-
-          <div className="chatbot-messages">
-            {messages.map((message, index) => (
-              <div
-                style={{ display: "flex", alignItems: "center" }}
-                key={index}
-              >
-                {index % 2 == 0 ? (
-                  <BsRobot
-                    style={{
-                      fontSize: "16px",
-                      color: "black",
-                      padding: "2px",
-                    }}
-                  />
-                ) : (
-                  ""
-                )}
-
-                <div
-                  className={`message ${
-                    message.isUser ? "user-message" : "bot-message"
-                  }`}
-                >
-                  {message.isUser ? (
-                    message.text.charAt(0).toUpperCase() + message.text.slice(1)
-                  ) : message.newMsg ? (
-                    <TextGenerateEffect words={message.text} />
-                  ) : (
-                    message.text
-                  )}
-                </div>
-                {index % 2 != 0 ? (
-                  <FaRegUser
-                    style={{
-                      fontSize: "16px",
-                      color: "black",
-                      display: "flex",
-                      marginLeft: "2px",
-                    }}
-                  />
-                ) : (
-                  ""
-                )}
-              </div>
-            ))}
-            <div ref={messagesEndRef} />
-          </div>
-
-          <div className="chatbot-input">
-            {!isSending && (
-              <div
-                style={{
-                  backgroundColor: "#00a053",
-                  borderRadius: "9999px",
-                  padding: "0.75rem",
-                  cursor: "pointer",
-                  transition: "all 0.2s ease-in-out",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  gap: "0.25rem",
-                  fontSize: "0.875rem",
-                }}
-                onClick={() =>
-                  listening
-                    ? SpeechRecognition.stopListening()
-                    : SpeechRecognition.startListening({ continuous: true })
-                }
-              >
-                {listening ? (
-                  <FaPause style={{ fontSize: "16px", color: "white" }} />
-                ) : (
-                  <FaMicrophone style={{ fontSize: "16px", color: "white" }} />
-                )}
-              </div>
-            )}
-
-            <input
-              type="text"
-              value={inputValue}
-              onChange={(e) =>
-                setInputValue(
-                  e.target.value.charAt(0).toUpperCase() +
-                    e.target.value.slice(1)
-                )
-              }
-              onKeyDown={handleKeyPress}
-              placeholder="Type your message..."
-              className="chat-input"
-              disabled={isSending}
-            />
-            <button
-              onClick={handleSendMessage}
-              className="send-button"
-              disabled={isSending || inputValue === ""}
+              <line y1="5" x2="39" y2="5" stroke="white" strokeWidth="4"></line>
+              <line
+                x1="12"
+                y1="1.5"
+                x2="26.0357"
+                y2="1.5"
+                stroke="white"
+                strokeWidth="3"
+              ></line>
+            </svg>
+            <svg
+              className="bin-bottom"
+              viewBox="0 0 33 39"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              {isSending ? (
-                <div className="spinner center">
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                  <div className="spinner-blade"></div>
-                </div>
-              ) : (
-                "Send"
-              )}
-            </button>
-          </div>
+              <mask id="path-1-inside-1_8_19" fill="white">
+                <path d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"></path>
+              </mask>
+              <path
+                d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+                fill="white"
+                mask="url(#path-1-inside-1_8_19)"
+              ></path>
+              <path d="M12 6L12 29" stroke="white" strokeWidth="4"></path>
+              <path d="M21 6V29" stroke="white" strokeWidth="4"></path>
+            </svg>
+          </button>
         </div>
-      )}
+
+        <div className="chatbot-messages">
+          {messages.map((message, index) => (
+            <div style={{ display: "flex", alignItems: "center" }} key={index}>
+              {index % 2 == 0 ? (
+                <BsRobot
+                  style={{
+                    fontSize: "16px",
+                    color: "black",
+                    padding: "2px",
+                  }}
+                />
+              ) : (
+                ""
+              )}
+
+              <div
+                className={`message ${
+                  message.isUser ? "user-message" : "bot-message"
+                }`}
+              >
+                {message.isUser ? (
+                  message.text.charAt(0).toUpperCase() + message.text.slice(1)
+                ) : message.newMsg ? (
+                  <TextGenerateEffect words={message.text} />
+                ) : (
+                  message.text
+                )}
+              </div>
+              {index % 2 != 0 ? (
+                <FaRegUser
+                  style={{
+                    fontSize: "16px",
+                    color: "black",
+                    display: "flex",
+                    marginLeft: "2px",
+                  }}
+                />
+              ) : (
+                ""
+              )}
+            </div>
+          ))}
+          <div ref={messagesEndRef} />
+        </div>
+
+        <div className="chatbot-input">
+          {!isSending && (
+            <div
+              style={{
+                backgroundColor: "#00a053",
+                borderRadius: "9999px",
+                padding: "0.75rem",
+                cursor: "pointer",
+                transition: "all 0.2s ease-in-out",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "0.25rem",
+                fontSize: "0.875rem",
+              }}
+              onClick={() =>
+                listening
+                  ? SpeechRecognition.stopListening()
+                  : SpeechRecognition.startListening({ continuous: true })
+              }
+            >
+              {listening ? (
+                <FaPause style={{ fontSize: "16px", color: "white" }} />
+              ) : (
+                <FaMicrophone style={{ fontSize: "16px", color: "white" }} />
+              )}
+            </div>
+          )}
+
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) =>
+              setInputValue(
+                e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
+              )
+            }
+            onKeyDown={handleKeyPress}
+            placeholder="Type your message..."
+            className="chat-input"
+            disabled={isSending}
+          />
+          <button
+            onClick={handleSendMessage}
+            className="send-button"
+            disabled={isSending || inputValue === ""}
+          >
+            {isSending ? (
+              <div className="spinner center">
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+                <div className="spinner-blade"></div>
+              </div>
+            ) : (
+              "Send"
+            )}
+          </button>
+        </div>
+      </div>
+      {/* )} */}
     </div>
   );
 };
 export default Chatbot;
+
+// <div>
+//   <button
+//     onClick={() => setIsVisible(!isVisible)}
+//     style={{
+//       fontSize: "20px",
+//       backgroundColor: "green",
+//       borderRadius: "50%",
+//       color: "white",
+//       position: "fixed",
+//       bottom: "12px",
+//       right: "12px",
+//       zIndex: 1000,
+//       width: "50px",
+//       height: "50px",
+//       border: "none",
+//       cursor: "pointer",
+//       display: "flex",
+//       alignItems: "center",
+//       justifyContent: "center",
+//     }}
+//   >
+//     {!isVisible ? "🤖" : "❌"}
+//   </button>
+//   <iframe
+//     ref={iframeRef}
+//     src="http://localhost:5174/"
+//     frameBorder="0"
+//     style={{
+//       position: "fixed",
+//       bottom: "3.3rem",
+//       right: "3.3rem",
+//       width: "350px",
+//       height: "450px",
+//       borderRadius: "12px",
+//       zIndex: 1000,
+//       display: isVisible ? "block" : "none",
+//       background: "transparent",
+//       pointerEvents: "auto",
+//     }}
+//   />
+// </div>
